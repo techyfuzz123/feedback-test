d55d96a2880f37aa7059bad2c5eefedb11d6c9d6
sumesh <sumeshrcse@gmail.com>
2022-10-12 19:49:04 +0530

    edited db.js for dburl

--- server/db.js
+++ server/db.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Variables
-const DB = process.env.DB
-const DB_URL = process.env.DB_URL + DB
+const DB = process.env.DB;
+const DB_URL = process.env.DB_URL;
 
 // connection to database
 module.exports = () => {
-    const connectionParams = {
-        useNewUrlParser: true
-    }
-    try {
-        mongoose.connect(DB_URL, connectionParams)
-        console.log(`connected to ${DB} database`)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  const connectionParams = {
+    useNewUrlParser: true,
+  };
+  try {
+    mongoose.connect(DB_URL, connectionParams);
+    console.log(`connected to ${DB} database`);
+  } catch (error) {
+    console.log(error);
+  }
+};
