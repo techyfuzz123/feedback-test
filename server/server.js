02d629ab07a0740ba86ff7d3b9d465382e61d558
sumesh <sumeshrcse@gmail.com>
2022-10-12 19:42:40 +0530

    edited corSoptions in server.js

--- server/server.js
+++ server/server.js
@@ -1,45 +1,44 @@
-const express = require('express');
-const app = express()
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const cors = require('cors');
-require('dotenv').config()
-const connection = require('./db')
-const student_routes = require('./routes/student-routes')
-const feedback_routes = require('./routes/feedback-routes')
-const auth_routes = require('./routes/auth-routes')
-const response_routes = require('./routes/response-routes')
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const connection = require("./db");
+const student_routes = require("./routes/student-routes");
+const feedback_routes = require("./routes/feedback-routes");
+const auth_routes = require("./routes/auth-routes");
+const response_routes = require("./routes/response-routes");
 
 // connecting to database
-connection()
+connection();
 
 // Variables
-const PORT = process.env.PORT || 8080       
+const PORT = process.env.PORT || 8080;
 const corsOptions = {
-    origin: 'http://192.168.0.109:3000',
-    optionsSuccessStatus: 200, // For legacy browser support
-    credentials :  true,
-    // methods: "GET, PUT, POST, DELETE"
-}
-
+  origin: process.env.FRONT_URL,
+  optionsSuccessStatus: 200, // For legacy browser support
+  credentials: true,
+  //  methods: "GET, PUT, POST, DELETE",
+};
 
 // middlewares
-app.use(cors(corsOptions))
-app.use(cookieParser())
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(express.json());
 
 // set routes
-app.use('/api/student', student_routes)
-app.use('/api/feedback', feedback_routes)
-app.use('/api/auth', auth_routes)
-app.use('/api/response', response_routes)
+app.use("/api/student", student_routes);
+app.use("/api/feedback", feedback_routes);
+app.use("/api/auth", auth_routes);
+app.use("/api/response", response_routes);
 
 // listening to port
 try {
-    app.listen(PORT, ()=>{
-        console.log(`listening on port ${PORT}....`)
-    })
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}....`);
+  });
 } catch (error) {
-    console.log(error)
+  console.log(error);
 }
