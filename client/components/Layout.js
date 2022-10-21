f2b1c230cf5599eb7a4cf693e6ffb3f3c50026be
sumesh <sumeshrcse@gmail.com>
2022-10-21 06:02:04 +0530

    installed a new nextjs in client after clearing old one

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,19 +1,15 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from "./Header";
+import Footer from "./Footer";
 
-export default function Layout(props) {
-    const {children } = props
+const Layout = (props) => {
+  const { children } = props;
   return (
-    // <div className='flex flex-col min-h-screen relative bg-slate-900 text-white'>
-    <div>
-      {/* <Header/> */}
-       <main > {/* className='flex-1 flex flex-col p-4' */}
-        {children}
-      </main>
-      {/* <Footer/> */}
+    <div className="flex flex-col min-h-screen items-center justify-center py-2">
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </div>
+  );
+};
 
-  )
-}
-
+export default Layout;
