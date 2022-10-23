392801cbb4ac921faa571241263993df439ba5fa
sumesh <sumeshrcse@gmail.com>
2022-10-23 11:36:51 +0530

    updated axios instance client

--- client/utils/axios-instance.js
+++ client/utils/axios-instance.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   withCredentials: true,
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  crossDomain: true,
 });
 
 module.exports = instance;
