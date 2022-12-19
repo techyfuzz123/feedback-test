f2e7f125c7ce5f9b967e56222882f8e6d65fd3b3
Tamilarasu Ubuntu <tamilarasu@ubuntu.com>
2022-12-19 16:02:23 +0530

    test

--- server/server.js
+++ server/server.js
@@ -67,7 +67,7 @@ const update_Image = (req, res, next) => {
   return res.status(500).json("something wrong");
 };
 
-//optionaltestthreefourfivesixserverseven
+//optionaltestthreefourfivesixserverseveneight
 // middlewares
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
