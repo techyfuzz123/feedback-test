e88ed2c364c8effa104e22a2043cd5bc8aa1c999
tamilarasu-g <tamilaguru08150@gmail.com>
2023-01-01 18:12:08 +0530

    server test

--- server/controllers/staff-controller.js
+++ server/controllers/staff-controller.js
@@ -7,7 +7,7 @@ const { Feedback } = require("../models/Feedback");
 
 const JWT_SECRET_KEY = process.env.JWT;
 /* 
-    This adadasdfassdasadadfadfaasdfasasdfaasdasdfasfffdaffController can add user to the users 
+    This adadasdfassdasadsdfasadfadfaasdfasasdfaasdasdfasfffdaffController can add user to the users 
     collection in the database
 */
 const addStaff = async (req, res) => {
