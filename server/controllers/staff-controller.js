b2841f7b6dd68e4187905969d16826ac4943dfa9
tamilarasu-g <tamilaguru08150@gmail.com>
2023-01-04 23:24:46 +0530

    webhook test

--- server/controllers/staff-controller.js
+++ server/controllers/staff-controller.js
@@ -7,7 +7,7 @@ const { Feedback } = require("../models/Feedback");
 
 const JWT_SECRET_KEY = process.env.JWT;
 /* 
-    This aasfasdasdfafafasdaasasdfadfdfafdadfsafsasdffsafdasdfassdasadsdfasadfadfaasdfasasdfaasdasdfasfffdaffController can add user to the users 
+    This aasasdfafasdasdfafafasdaasasdfadfdfafdadfsafsasdffsafdasdfassdasadsdfasadfadfaasdfasasdfaasdasdfasfffdaffController can add user to the users 
     collection in the database
 */
 const addStaff = async (req, res) => {
